0492259cf9b536f22c6f753a1973f2215353c315
vencax <vencax@centrum.cz>
2021-02-16 14:30:47 +0100

    jeste index :)

--- template/index.js
+++ template/index.js
@@ -3,6 +3,7 @@ import imageGrid from './components/imageGrid.js'
 import newsPreview from './components/newsPreview.js'
 import expandable from './components/expandable.js'
 import carousel from './components/carousel.js'
+import masonry from './components/masonry.js'
 
 import defaultLayout from './layout/default.js'
 import pageLayout from './layout/page.js'
@@ -16,5 +17,6 @@ Vue.component('imageGrid', imageGrid)
 Vue.component('newsPreview', newsPreview)
 Vue.component('expandable', expandable)
 Vue.component('carousel', carousel)
+Vue.component('masonry', masonry)
 
 export default { hero, imageGrid, newsPreview }
