1318e2dd58d503db4d3be3491cbf6d159bb0675b
vencax <vencax@centrum.cz>
2021-02-18 09:53:26 +0100

    nej z programu v cards

--- data/_index.js
+++ data/_index.js
@@ -101,6 +101,7 @@ export default {
         },
         {
           "component": "MDText",
+          "class": "py-5",
           "content": "## O Táborských setkání \n\nHistorické centrum Tábora s několika náměstími, křivolakými uličkami i bohatou zelení za městskými hradbami ožívá na tři dny dávnou historií první poloviny 15. století. \nČasem, kdy se vlády nad městem ujal husitský vojevůdce Jan Žižka z Trocnova. \nTaké nyní, po téměř šesti stovkách let každoročně symbolicky přebírá slavný hejtman vládu nad Táborem. \nZa rachotu bubnů a světel pochodní ho přitom doprovází zástup jeho husitských bojovníků.\nPo slavnostním zahájení si už stačí vybrat, zda zavítáte ke stánku s medovinou a chlebem se škvarky, nebo dáte přednost vybraným středověkým lahůdkám v některé z útulných taveren. Programový „jídelníček“ je také nepřeberný – ohňostroj; průvody v historických kostýmech; středověký jarmark; žebrácká ulička; koncerty duchovní, středověké, ale i současné hudby; vystoupení skupin historického šermu; rytířský turnaj na koních; divadla pod širým nebem; výstavy; hry a soutěže pro děti. \nTo a mnohem víc vám nabízí Táborská setkání…\n\n### Sponzoři"
         },
         {
--- template/components/nej_z_programu.js
+++ template/components/nej_z_programu.js
@@ -2,9 +2,9 @@ export default {
   data: function () {
     return {
       posts: [
-        { title: 'Vystoupeni 1', den: 'patek', hodina: '20:00', content: 'kratke intro vystoupeni' },
-        { title: 'Vystoupeni 2', den: 'patek', hodina: '14:00', content: 'kratke intro vystoupeni2' },
-        { title: 'Vystoupeni 3', den: 'sobota', hodina: '16:00', content: 'kratke intro vystoupeni3' }
+        { title: 'Vystoupeni 1', obrazek: 'http://data.vxk.cz/ts/wp-content/uploads/2020/09/1-16.jpg', den: 'patek', hodina: '20:00', content: 'kratke intro vystoupeni' },
+        { title: 'Vystoupeni 2', obrazek: 'http://data.vxk.cz/ts/wp-content/uploads/2020/09/1-16.jpg', den: 'patek', hodina: '14:00', content: 'kratke intro vystoupeni2' },
+        { title: 'Vystoupeni 3', obrazek: 'http://data.vxk.cz/ts/wp-content/uploads/2020/09/1-16.jpg', den: 'sobota', hodina: '16:00', content: 'kratke intro vystoupeni3' }
       ]
     }
   },
@@ -12,9 +12,16 @@ export default {
   template: `
   <div class="row">
     <div v-for="(i, idx) in posts" :key="idx" class="col">
-      <h3>{{ i.title }}</h3>
-      <h4>{{ i.den }} - <i>{{ i.hodina }}</i></h4>
-      <p>{{ i.content }}</p>
+      <div class="card">
+        <img :src="i.obrazek" class="card-img-top" :alt="i.title">
+        <div class="card-body">
+          <h5 class="card-title">{{ i.title }}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">
+            {{ i.den }} - <i>{{ i.hodina }}</i>
+          </h6>
+          <p class="card-text">{{ i.content }}</p>
+        </div>
+      </div>
     </div>
   </div>
   `
