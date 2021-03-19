0a657c5f8e955f8e8eb6e6216eee6ed5fc2cebd9
vencax <vencax@centrum.cz>
2021-03-19 12:12:10 +0100

    markdown component

--- vuecustoms.js
+++ vuecustoms.js
@@ -3,6 +3,19 @@ import composition from './components/composition.js'
 import MDText from './components/mdText.js'
 Vue.component('MDText', MDText)
 
+Vue.component('markdown', {
+  props: ['component', 'text'],
+  computed: {
+    html: function() {
+      return this.$props.text.indexOf('\n') > 0
+        ? marked(this.$props.text)
+        : marked.parseInline(this.$props.text)
+    }
+  },
+  template: '<span :is="component" v-html="html" />'
+})
+
+moment.locale('cs')
 Vue.filter('formatDate', function (value) {
   if (value) {
     value = _.isString(value) ? moment(value) : value
