1b821397204819556ee4060c00b915390167cf51
vencax <vencax@centrum.cz>
2021-05-13 12:41:53 +0200

    root template renders only router-view

--- components/page.js
+++ components/page.js
@@ -11,41 +11,48 @@ function findComponents(data, components) {
   }, components)   
 }
 
-export default async function (path, dataUrl) {
-  const dataReq = await axios.get(dataUrl + path)
-  const data = jsyaml.load(dataReq.data)
-  const templateReq = await axios.get(dataUrl + 'template/layout/' + data.layout + '.html')
-  const components = findComponents(data, [])
-
+export default function pageCreator (dataUrl) {
   function loadComponent(name) {
     const url = dataUrl + 'template/components/' + name + '.js'
     return import(url)
   }
+  // load header and footer
+  Vue.component('pageHeader', () => loadComponent('header'))
+  Vue.component('pageFooter', () => loadComponent('header'))
 
-  // zatim global registrace
-  components.map(name => {
-    Vue.component(name, () => loadComponent(name))
-  })
+  return async function (path) {
+    const dataReq = await axios.get(dataUrl + path)
+    const data = jsyaml.load(dataReq.data)
+    const templateReq = await axios.get(dataUrl + 'template/layout/' + data.layout + '.html')
+    const components = findComponents(data, [])
 
-  return {
-    data: () => ({ data, path: null }),
-    created: function () {
-      this.$data.path = this.$router.currentRoute.path
-    },
-    computed: {
-      components: function () {
-        return _.filter(this.$data.data.children, i => (!i.disabled))
-      }
-    },
-    metaInfo () {
-      return {
-        title: this.$data.data.title,
-        meta: [
-          { vmid: 'description', name: 'description', content: this.$data.data.desc },
-          { vmid: 'keywords', name: 'keywords', content: this.$data.data.keywords }
-        ]
-      }
-    },
-    template: templateReq.data
+    
+
+    // zatim global registrace
+    components.map(name => {
+      Vue.component(name, () => loadComponent(name))
+    })
+
+    return {
+      data: () => ({ data, path: null }),
+      created: function () {
+        this.$data.path = this.$router.currentRoute.path
+      },
+      computed: {
+        components: function () {
+          return _.filter(this.$data.data.children, i => (!i.disabled))
+        }
+      },
+      metaInfo () {
+        return {
+          title: this.$data.data.title,
+          meta: [
+            { vmid: 'description', name: 'description', content: this.$data.data.desc },
+            { vmid: 'keywords', name: 'keywords', content: this.$data.data.keywords }
+          ]
+        }
+      },
+      template: templateReq.data
+    }
   }
 }
--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 /* global Vue, VueRouter */
 import './vuecustoms.js'
 import Store from './store.js'
-import Page from './components/page.js'
+import PageCreator from './components/page.js'
 
 export default async function init (mountpoint, routesUrl, dataUrl) {
   const reqs = await Promise.all([
     axios(routesUrl),
     axios(dataUrl + 'config.json')
   ])
+  const pageCreator = PageCreator(dataUrl)
   const webRoutes = _.map(reqs[0].data, i => {
-    return { path: i.path, component: () => Page(i.data, dataUrl) }
+    return { path: i.path, component: () => pageCreator(i.data) }
   })
   const siteconf = reqs[1].data
   
@@ -22,23 +23,13 @@ export default async function init (mountpoint, routesUrl, dataUrl) {
   new Vue({
     router,
     store,
-    components: { 
-      pageHeader: () => import(dataUrl + 'template/components/header.js'), 
-      pageFooter: () => import(dataUrl + 'template/components/footer.js')
-    },
     metaInfo: {
       // if no subcomponents specify a metaInfo.title, this title will be used
       title: siteconf.defaulttitle || '--',
       // all titles will be injected into this template
       titleTemplate: `%s | ${siteconf.title}`
     },
-    template: `
-    <div>
-      <pageHeader />
-      <router-view :key="$route.fullPath" />
-      <pageFooter />
-    </div>
-    `
+    template: `<router-view :key="$route.fullPath" />`
   }).$mount(mountpoint)
 }
 
