8e233afbe3be72bd59ef04ff87803eff879d6bb7
vencax <vencax@centrum.cz>
2021-05-22 18:43:36 +0200

    new ver

--- dist/bbb-web.js
+++ dist/bbb-web.js
@@ -101,8 +101,74 @@ var initBBBWeb = (function () {
     template: '<span :is="c" v-html="html" @click="handleClicks" />'
   };
 
+  const pageList = {
+    props: ['pages'],
+    template: `
+  <ul class="sitemaplist">
+    <li v-for="i, idx in pages" :key="idx">
+      <router-link :to="i.id"><h5>{{ i.title }}</h5></router-link>
+      <span>{{ i.desc }}</span>
+      <sitemapPageList v-if="i.children" :pages="i.children" />
+    </li>
+  </ul>
+  `
+  };
+
+  var sitemap = {
+    data: function () {
+      return {
+        tree: null,
+        loading: true
+      }
+    },
+    created: async function () {
+      try {
+        const reqs = await Promise.all([
+          axios.get(`${this.$store.state.site.serviceUrl}routes.json`),
+          axios.get(`${this.$store.state.site.serviceUrl}metainfo.json`)
+        ]);
+        this.$data.tree = _buildTree(reqs[0].data, reqs[1].data);
+        this.$data.loading = false;
+      } catch (_) {
+      }
+      Vue.component('sitemapPageList', pageList);
+    },
+    props: ['data'],
+    template: `
+  <i v-if="loading" class="fas fa-spinner fa-spin"></i>
+  <sitemapPageList v-else :pages="tree" />
+  `
+  };
+
+  function _buildTree(pages, metas) {
+    function _insert2Tree (node, subtree, path) {
+      const existing = _.find(subtree, i => i.foldername === path[0]);
+      if (existing && path.length > 1) {
+        existing.children = existing.children || [];
+        _insert2Tree(node, existing.children, _.rest(path));
+      } else {
+        const meta = metas[node.data];
+        subtree.push({
+          id: node.path,
+          title: meta.title,
+          desc: meta.desc,
+          foldername: path[0]
+        });
+      }
+    }
+    const sorted = _.sortBy(pages, 'path');
+    const meta = metas['index.yaml'];
+    const tree = [{ id: '/', title: meta.title, desc: meta.desc, foldername: '' }];
+    _.map(_.rest(sorted), i => {
+      const parts = i.path.split('/');
+      _insert2Tree(i, tree, parts);
+    });
+    return tree
+  }
+
   /* global Vue, _, moment */
   Vue.component('MDText', MDText);
+  Vue.component('sitemap', sitemap);
   Vue.component('markdown', markdown);
 
   moment.locale('cs');
@@ -184,7 +250,7 @@ var initBBBWeb = (function () {
     }
   })};
 
-  const ALREADY_REGISTERED = ['composition', 'MDText'];
+  const ALREADY_REGISTERED = ['composition', 'MDText', 'sitemap'];
   const _loaded = {};
 
   function findComponents(data, components) {
@@ -255,12 +321,13 @@ var initBBBWeb = (function () {
 
   /* global Vue, VueRouter */
 
-  async function init (mountpoint, routesUrl, dataUrl) {
+  async function init (mountpoint, serviceUrl, dataUrl) {
     const reqs = await Promise.all([
-      axios(routesUrl),
+      axios(serviceUrl + 'routes.json'),
       axios(dataUrl + 'config.json')
     ]);
     const siteconf = reqs[1].data;
+    Object.assign(siteconf, { serviceUrl, dataUrl });
     const pageCreator$1 = pageCreator(dataUrl, siteconf);
     const webRoutes = _.map(reqs[0].data, i => {
       return { path: i.path, component: () => pageCreator$1(i.data) }
