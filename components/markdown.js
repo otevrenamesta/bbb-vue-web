8fe4fe0e0221f0c16bee3289481003f055c5def0
vencax <vencax@centrum.cz>
2022-03-04 15:03:34 +0100

    fix

--- components/markdown.js
+++ components/markdown.js
@@ -3,17 +3,16 @@ import linksHijack from './link_hijack.js'
 export function renderMD (content, self) {
   const renderer = new marked.Renderer()
   // const originalLinkRenderer = renderer.link.bind(renderer)
+  function isLocal (href) {
+    return href.indexOf('http') !== 0 
+    && href.indexOf('.') < 0 // jen odkazy typu /a/b/c/d
+    && href.indexOf('mailto') !== 0
+  }
   renderer.link = (href, title, text) => {
-    if (href.indexOf('http') !== 0 
-      && href.indexOf('.') >= 0 
-      && href.indexOf('mailto') !== 0) {
-      const url = `${self.$root.cdn}/${href}`
-      const icon = '<i class="fas fa-external-link-alt"></i>'
-      return `<a href="${url}" target="_blank">${text} ${icon}</a>`
-    } else {
-      // return originalLinkRenderer(href, title, text)
-      return `<a href="${href}" l="local">${text}</a>`
-    }
+    if (isLocal(href)) return `<a href="${href}" l="local">${text}</a>`
+    const url = href.indexOf('http') === 0 ? href : `${self.$root.cdn}/${href}`
+    const icon = '<i class="fas fa-external-link-alt"></i>'
+    return `<a href="${url}" target="_blank">${text} ${icon}</a>`
   }
   return content.indexOf('\n') > 0 
     ? marked(content, { renderer })
