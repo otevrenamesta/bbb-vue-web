8791964733cb812e0a16954f0b7d55256cc2b591
vencax <vencax@centrum.cz>
2022-04-14 15:21:40 +0200

    fix

--- components/markdown.js
+++ components/markdown.js
@@ -1,16 +1,12 @@
 import linksHijack from './link_hijack.js'
 const asIsHrefRegex = /^https?:[/]{2}|mailto:|tel:/gm
+const localHref = /^(\/\w+)+\/?/i // jen odkazy typu /a/b/c/d e.g. /galerie/2021
 
 export function renderMD (content, self) {
   const renderer = new marked.Renderer()
   // const originalLinkRenderer = renderer.link.bind(renderer)
-  function isLocal (href) {
-    return href.indexOf('http') !== 0 
-    && href.indexOf('.') < 0 // jen odkazy typu /a/b/c/d
-    && href.indexOf('mailto') !== 0
-  }
   renderer.link = (href, title, text) => {
-    if (isLocal(href)) return `<a href="${href}" l="local">${text}</a>`
+    if (href.match(localHref)) return `<a href="${href}" l="local">${text}</a>`
     const url = href.match(asIsHrefRegex) ? href : `${self.$root.cdn}/${href}`
     const icon = '<i class="fas fa-external-link-alt"></i>'
     return `<a href="${url}" target="_blank">${text} ${icon}</a>`
